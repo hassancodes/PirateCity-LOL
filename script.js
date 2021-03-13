a8a4336b696e7b4f1709fa2d2cf3aa312bc755d1
hassancodes <59165844+hassancodes@users.noreply.github.com>
2021-03-13 17:32:14 -0500

    commit

--- script.js
+++ script.js
@@ -7,13 +7,27 @@ var mybtn = document.getElementById("btn");
 mybtn.addEventListener("click", myfunc);
 
 
-
-
 // function section
 function myfunc(){
     location.href = "main.html";
 }
 
 
+// main riddle part . For "main.html"
+
+riddle = {
+
+    "What pet does a pirate carry on his shoulder?" : "Parrot", 
+    "What hath but three feet, three hands, and three eyes?":"two pirates",
+    "Where do you take a sick pirate ship?":  "to the dock"
+}
+
+
+var a = document.getElementById("div__button");
+a.addEventListener("click", LOL);
+
+function LOL(){
+    console.log("Bye. bsdk");
+}
 
 
