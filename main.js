0aec47a6772281bf2acde05434a6dbb968d6cb7b
hassancodes <59165844+hassancodes@users.noreply.github.com>
2021-03-13 21:36:20 -0500

    adding code

---
+++ main.js
@@ -0,0 +1,31 @@
+// main riddle part . For "main.html"
+var a = document.getElementById("Main").addEventListener("click", riddleFunction);
+var counter =0;
+
+
+console.log("LOL");
+riddle = [
+
+    {"What pet does a pirate carry on his shoulder?" : "Parrot"},
+    {"What hath but three feet, three hands, and three eyes?":"two pirates"},
+    {"Where do you take a sick pirate ship?":  "to the dock"},
+
+]
+
+
+
+
+function riddleFunction(){
+
+    for(key in riddle[counter]){
+        if(riddle[counter].hasOwnProperty(key)){
+            document.getElementById("riddle-card").innerHTML = key;
+        }
+    }
+    if (counter==riddle.length){
+    counter-=riddle.length;    
+    }else{
+        counter +=1
+    }; 
+}
+
--- script.js
+++ script.js
@@ -10,26 +10,7 @@ mybtn.addEventListener("click", myfunc);
 // function section
 function myfunc(){
     location.href = "main.html";
-}
-
-
-// main riddle part . For "main.html"
-
-riddle = {
-
-    "What pet does a pirate carry on his shoulder?" : "Parrot", 
-    "What hath but three feet, three hands, and three eyes?":"two pirates",
-    "Where do you take a sick pirate ship?":  "to the dock"
-}
-
-
-var a = document.getElementById("div__button");
-a.addEventListener("click", LOL);
-
-function LOL(){
-
-    console.log("Bye. bsdk");
-
+    alert("There is no turning BACK🔪🏴‍☠️")
 }
 
 
