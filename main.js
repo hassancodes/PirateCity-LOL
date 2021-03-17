3e45cc8440f2c127fb256e9bf164749f7b1e32cb
hassancodes <59165844+hassancodes@users.noreply.github.com>
2021-03-17 10:52:17 -0400

    commit

--- main.js
+++ main.js
@@ -1,31 +1,36 @@
 // main riddle part . For "main.html"
 var a = document.getElementById("Main").addEventListener("click", riddleFunction);
-var counter =0;
+var counter = 0;
 
 
 console.log("LOL");
 riddle = [
 
-    {"What pet does a pirate carry on his shoulder?" : "Parrot"},
-    {"What hath but three feet, three hands, and three eyes?":"two pirates"},
-    {"Where do you take a sick pirate ship?":  "to the dock"},
+    {
+        "What pet does a pirate carry on his shoulder?": "Parrot"
+    },
+    {
+        "What hath but three feet, three hands, and three eyes?": "two pirates"
+    },
+    {
+        "Where do you take a sick pirate ship?": "to the dock"
+    },
 
 ]
 
 
 
 
-function riddleFunction(){
+function riddleFunction() {
 
-    for(key in riddle[counter]){
-        if(riddle[counter].hasOwnProperty(key)){
+    for (key in riddle[counter]) {
+        if (riddle[counter].hasOwnProperty(key)) {
             document.getElementById("riddle-card").innerHTML = key;
         }
     }
-    if (counter==riddle.length){
-    counter-=riddle.length;    
-    }else{
-        counter +=1
-    }; 
+    if (counter == riddle.length) {
+        counter -= riddle.length;
+    } else {
+        counter += 1;
+    };
 }
-
