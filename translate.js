058578f2baf73eaf5029217aeba5da11f3dae544
hassancodes <59165844+hassancodes@users.noreply.github.com>
2021-05-15 12:56:57 -0400

    last commit

---
+++ translate.js
@@ -0,0 +1,39 @@
+  
+// declaring variables. attaching to ids.
+var btnTranslate = document.querySelector("#trans-button");
+var intext = document.querySelector(".in-text-area");
+var outtext = document.querySelector(".out-text-area");
+
+
+var serverUrl = "https://api.funtranslations.com/translate/pirate.json"
+
+
+
+function createUrl(input){
+    var url = serverUrl+ "?text="+ input;
+    return url;
+}
+
+
+// error handler
+
+function errorHandler(error){
+    console.log("error occured",error);
+    alert("something is wrong with the server");
+}
+
+
+function clickHandler(){
+    var inputText = intext.value;
+    var urlReady = createUrl(inputText);
+
+
+    fetch(urlReady).then(response => response.json()).then(json => {
+        var translatedText = json.contents.translated;
+        outtext.innerText = translatedText; // output
+       })
+.catch(errorHandler)
+
+};
+
+btnTranslate.addEventListener("click",clickHandler)
